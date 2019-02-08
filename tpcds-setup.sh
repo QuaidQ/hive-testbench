24e56daa46baae155b124d3e1560a639b77a0d4f
QuaidQ <qquaid75@gmail.com>
2019-02-08 15:39:16 -0500

    Update tpcds-setup.sh

--- tpcds-setup.sh
+++ tpcds-setup.sh
@@ -70,7 +70,7 @@ hadoop fs -chmod -R 777  ${DIR}/${SCALE}
 
 echo "TPC-DS text data generation complete."
 
-HIVE="beeline -n hive -u 'jdbc:hive2://zk1-qq19.uy0lzlw1o13epfvrrb40bid0lc.cx.internal.cloudapp.net:2181,zk3-qq19.uy0lzlw1o13epfvrrb40bid0lc.cx.internal.cloudapp.net:2181,zk6-qq19.uy0lzlw1o13epfvrrb40bid0lc.cx.internal.cloudapp.net:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2' "
+HIVE="beeline -n hive -u 'jdbc:hive2://zk0-qq19.oi3m1hy3gtue5gfzi3ezwbldrd.cx.internal.cloudapp.net:2181,zk1-qq19.oi3m1hy3gtue5gfzi3ezwbldrd.cx.internal.cloudapp.net:2181,zk4-qq19.oi3m1hy3gtue5gfzi3ezwbldrd.cx.internal.cloudapp.net:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2' "
 
 # Create the text/flat tables as external tables. These will be later be converted to ORCFile.
 echo "Loading text data into external tables."
